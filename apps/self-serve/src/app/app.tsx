d4f369013c6d10ffce7535ec0534c00ad9d6e5c6
Buddhadeb Das <buddhadeb.das@cleartax.in>
2022-12-06 11:08:29 +0530

    consuming Ui

--- apps/self-serve/src/app/app.tsx
+++ apps/self-serve/src/app/app.tsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import {Ui} from '@myorg/ui';
 
 export function App() {
   return (
     <>
+      <Ui/>
       <NxWelcome title="self-serve" />
       <div />
     </>
